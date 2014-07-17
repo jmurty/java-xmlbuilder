7c131b989254adb89fd436a27f9d6019dc864296
James Murty <james@murty.co>
2014-07-17 14:42:49 +0100

    Add unit tests for XMLBuilder2 exception wrapping feature, re #4

--- src/test/java/com/jamesmurty/utils/TestXMLBuilder2.java
+++ src/test/java/com/jamesmurty/utils/TestXMLBuilder2.java
@@ -1,5 +1,8 @@
 package com.jamesmurty.utils;
 
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+
 public class TestXMLBuilder2 extends BaseXMLBuilderTests {
 
     @Override
@@ -10,6 +13,30 @@ public class TestXMLBuilder2 extends BaseXMLBuilderTests {
     @Override
     protected boolean isRuntimeExceptionsOnly() {
         return true;
+    }
+
+    // NOTE: No checked exceptions for API calls made in this test method
+    public void testNoCheckedExceptions() {
+        XMLBuilder2 builder = XMLBuilder2.create("Blah");
+        builder = XMLBuilder2.parse(EXAMPLE_XML_DOC);
+        builder.stripWhitespaceOnlyTextNodes();
+        builder.asString();
+        builder.elementAsString();
+        builder.xpathQuery("/*", XPathConstants.NODESET);
+        builder.xpathFind("/Projects");
+    }
+
+    public void testExceptionWrappedInXMLBuilderRuntimeException() {
+        XMLBuilder2 builder = XMLBuilder2.parse(EXAMPLE_XML_DOC);
+        try {
+            builder.xpathFind("/BadPath");
+            fail("Expected XMLBuilderRuntimeException");
+        } catch (XMLBuilderRuntimeException e) {
+            assertEquals(XMLBuilderRuntimeException.class, e.getClass());
+            Throwable cause = e.getCause();
+            assertEquals(XPathExpressionException.class, cause.getClass());
+            assertTrue(cause.getMessage().contains("does not resolve to an Element"));
+        }
     }
 
 }
