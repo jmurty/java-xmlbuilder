7dbf6d33a78b426af654419b561ba3b61d75714e
James Murty <james@murty.co>
2014-07-17 11:02:42 +0100

    Remove unused import

--- src/main/java/com/jamesmurty/utils/XMLBuilder.java
+++ src/main/java/com/jamesmurty/utils/XMLBuilder.java
@@ -52,7 +52,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
